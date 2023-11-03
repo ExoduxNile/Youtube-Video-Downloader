e93c867c5c281cc8c7b8e61f9f2321bd2b700950
ExoduxNile <sandeep.tete2012@gmail.com>
2023-11-03 10:35:35 +0530

    Update server.py

--- api/server.py
+++ api/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, Response
+from flask import Flask, render_template, request, redirect, Response, jsonify
 import csv
 import datetime
 import os
