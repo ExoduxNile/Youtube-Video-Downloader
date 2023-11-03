e0bbbd320af9ceb6080ce6f20fe918df74d56943
ExoduxNile <sandeep.tete2012@gmail.com>
2023-11-03 13:12:48 +0530

    check

--- api/ytd.py
+++ api/ytd.py
@@ -1,10 +1,19 @@
 import yt_dlp
-import sys
+from flask import Flask, request, render_template
+app = Flask(__name__)
 
-def download_video(url):
-    options = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' 
-    }
+@app.route('/', methods=['GET', 'POST'])
+def download_video():
+    if request.method == 'POST':
+        url = request.form['url']
+        
+        options = {
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'
+        }
 
-    with yt_dlp.YoutubeDL(options) as ydl:
-        ydl.download([url])
+        with yt_dlp.YoutubeDL(options) as ydl:
+            ydl.download([url])
+
+        return "Video downloaded!"
+
+    return render_template('form.html')
