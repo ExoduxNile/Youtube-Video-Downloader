486f31d53741d3022083319dfec8dd67909f3023
ExoduxNile <sandeep.tete2012@gmail.com>
2023-11-03 20:09:23 +0530

    Update server.py

--- api/server.py
+++ api/server.py
@@ -47,21 +47,27 @@ def about():
 
 
 
-@app.route('/download', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])  
 def download_video():
-    if request.method == 'POST':
-        url = request.form['url']
-        
-        options = {
-            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'
-        }
 
-        with yt_dlp.YoutubeDL(options) as ydl:
-            ydl.download([url])
+  if request.method == 'POST':
+    
+    url = request.form['url']
+    
+    # Build ffmpeg command 
+    command = ['node', 'ffmpeg.js', 
+      '-i', url,
+      '-c:v', 'libx264', 
+      '-crf', '28',
+      'output.mp4'
+    ]
+    
+    # Execute ffmpeg.js script 
+    subprocess.run(command)
 
-        return "Video downloaded!"
+    return "Video downloaded!"
 
-    return render_template('index.html')
+  return render_template('index.html')
 
 
 
