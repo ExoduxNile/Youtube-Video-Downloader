d03f4c9aaeaa360b135fdd3f4bcbd6e24ce3b235
ExoduxNile <sandeep.tete2012@gmail.com>
2023-11-03 20:24:33 +0530

    Update server.py

--- api/server.py
+++ api/server.py
@@ -47,32 +47,6 @@ def about():
 
 
 
-@app.route('/', methods=['GET', 'POST'])  
-def download_video():
-
-  if request.method == 'POST':
-    
-    url = request.form['url']
-    
-    # Build ffmpeg command 
-    command = ['node', 'ffmpeg.js', 
-      '-i', url,
-      '-c:v', 'libx264', 
-      '-crf', '28',
-      'output.mp4'
-    ]
-    
-    # Execute ffmpeg.js script 
-    subprocess.run(command)
-
-    return "Video downloaded!"
-
-  return render_template('index.html')
-
-
-
-
-
 
 @app.route('/robots.txt')
 def noindex():
