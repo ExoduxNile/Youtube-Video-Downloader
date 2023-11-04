31e10d6b23dbc9eecba4afe4780f9e7abb7902f6
ExoduxNile <sandeep.tete2012@gmail.com>
2023-11-04 09:50:47 +0530

    changes

---
+++ .vercel/cache/server/api/ytd.py
@@ -0,0 +1,21 @@
+import yt_dlp
+import ffmpeg
+
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def download_video():
+    if request.method == 'POST':
+        url = request.form['url']
+        
+        options = {
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'
+        }
+
+        with yt_dlp.YoutubeDL(options) as ydl:
+            ydl.download([url])
+
+        return "Video downloaded!"
+
+    return render_template('form.html')
