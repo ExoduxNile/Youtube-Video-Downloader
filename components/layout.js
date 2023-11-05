6eee636230d0d60f5ae590147fa96d6c3862501f
ExoduxNile <sandeep.tete2012@gmail.com>
2023-11-05 15:43:03 +0530

    changes

---
+++ components/layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <header>
+        // navigation
+      </header>
+      {children}
+      <footer>
+        // footer content
+      </footer>
+    </>
+  )
+}
---
+++ pages/about.jsx
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout' 
+import styles from '../styles/About.module.css'
+import React from 'react';
+
+export default function About() {
+  return (
+    <Layout>
+      <main className={styles.main}>
+        <h1>About Me</h1>
+        // about page content 
+      </main>
+    </Layout>
+  )
+}
